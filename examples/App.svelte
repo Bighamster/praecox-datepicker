601e8ed402a65696593b3df2072bb51a4aff5ebf
Bighamster <vpodgornyy@gmail.com>
2021-03-28 20:02:48 +0300

    wip

--- examples/App.svelte
+++ examples/App.svelte
@@ -2,30 +2,26 @@
   import Datepicker from "../src/Calendar.svelte";
   import { isPresent } from "./tools.js";
 
-  // let selected = new Date();
-  let current_date = "2021-02-18";
+  let selected = (new Date("2021-06-5")).getTime();
 
-  let nowDate; // = current_date || new Date();
-  let selected;
-
-  const handleChangeDate = e => current_date = new Date(e.detail);
+  const handleChangeDate = e => console.log('handleChangeDate', new Date(e.detail));
 
   // $: console.log('selected', selected)
-  $: if( isPresent(selected) ) current_date = new Date(selected)
-  $: console.log('current_date', current_date)
-  $: console.log('App.nowDate', nowDate)
+  // $: if( isPresent(selected) ) current_date = new Date(selected)
+  $: console.log('selected', selected)
 
 </script>
 
 <h1>Test 5</h1>
 
-<button on:click={ e => nowDate = "2021-01-30" }>Assign nowDate</button>
+<button on:click={ e => selected = (new Date("2021-03-31")).getTime() }>Assign selected</button>
 
 <div class="wrapper">
 	<Datepicker
     lang="ru"
-    {nowDate}
-    on:change={ handleChangeDate }
+    nowDate={ selected }
+    bind:selected
+    on:change = { handleChangeDate }
 	/>
 </div>
 
--- examples/rollup.config.js
+++ examples/rollup.config.js
@@ -1,6 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import livereload from "rollup-plugin-livereload";
+import commonjs from '@rollup/plugin-commonjs';
+import css from 'rollup-plugin-css-only';
 import serve from "rollup-plugin-serve";
 
 export default {
@@ -14,9 +16,19 @@ export default {
   },
   plugins: [
     svelte({
-      include: ['App.svelte', '../src/**/*.svelte'],
+      compilerOptions: {
+				// enable run-time checks when not in production
+				dev: false
+			},
+      emitCss: !true,
+      include: ['App.svelte', '../src/**/*.svelte']
     }),
-    resolve(),
+    css({ output: 'bundle.css' }),
+    resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+    commonjs(),
     serve("public"),
     livereload({ watch: "public" })
   ]
